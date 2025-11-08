292daad6b302702a7bdef6ade12d95e99b9eb5a4
PautovaMO <Pautova812007@mail.ru>
2025-11-08 22:37:18 +0500

    Task3

---
+++ Tyuiu.PautovaMO.Sprint4.Task3.V23.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using Tyuiu.PautovaMO.Sprint4.Task3.V23.Lib;
+
+namespace Tyuiu.PautovaMO.Sprint4.Task3.V23.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            int[,] arrау = { { 7, 5, 5, 6, 7 },{ 8, 7, 8, 4, 3 },{ 5, 6, 7, 8, 3},{ 4, 2, 3, 6, 4 },{ 5, 2, 4, 2, 3 } };
+            int res = ds.Calculate(arrау);
+            int wait = 13;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.PautovaMO.Sprint4.Task3.V23/Program.cs
@@ -0,0 +1,52 @@
+﻿using System.Net;
+using Tyuiu.PautovaMO.Sprint4.Task3.V23.Lib;
+
+namespace Tyuiu.PautovaMO.Sprint4.Task3.V23
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] arr =new int [5,5] { { 7, 5, 5, 6, 7 }, { 8, 7, 8, 4, 3 }, { 5, 6, 7, 8, 3 }, { 4, 2, 3, 6, 4 }, { 5, 2, 4, 2, 3 } };
+            
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #4 | Выполнил : Паутова М.О. | ИБКСб-25-1";
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #4                                                               *");
+            Console.WriteLine("* Тема:               массив                                              *");
+            Console.WriteLine("* Задание #3                                                              *");
+            Console.WriteLine("* Вариант #23                                                             *");
+            Console.WriteLine("* Выполнил: Паутова Мария Олеговна | ИБКСб-25-1                           *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("*  подсчитать произведение четных элементов массива.                      *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+
+            for (int i = 0; i < arr.GetLength(0); i++)
+            {
+                for (int j = 0; j < arr.GetLength(1); j++)
+                {
+                    Console.WriteLine($"{arr[i, j]}\t");
+
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            int res = ds.Calculate(arr);
+            Console.WriteLine("Колличество = " + res);
+
+
+            Console.ReadKey();
+        }
+    }
+}
