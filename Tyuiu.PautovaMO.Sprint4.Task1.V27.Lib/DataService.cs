7b35401fc720f821ff82682ac0131a190244d30f
PautovaMO <Pautova812007@mail.ru>
2025-11-08 18:40:48 +0500

    Task1

---
+++ Tyuiu.PautovaMO.Sprint4.Task1.V27.Lib/DataService.cs
@@ -0,0 +1,26 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using tyuiu.cources.programming.interfaces.Sprint4;
+namespace Tyuiu.PautovaMO.Sprint4.Task1.V27.Lib
+{
+    public class DataService : ISprint4Task1V27
+    {
+
+        public int Calculate(int[] array)
+        {
+            // Переменная для суммы нечетных элементов
+            int s = 1;
+
+            // Проходим по всем элементам массива
+            for (int i = 0; i < array.Length; i++)
+            {
+                // Проверяем, является ли элемент нечетным
+                if (array[i] % 2 == 0)
+                {
+                    s= s* array[i]; 
+                }
+            }
+            return s;
+        }
+
+    }
+}
---
+++ Tyuiu.PautovaMO.Sprint4.Task1.V27/Program.cs
@@ -0,0 +1,56 @@
+﻿using System.Net;
+using Tyuiu.PautovaMO.Sprint4.Task1.V27.Lib;
+
+namespace Tyuiu.PautovaMO.Sprint4.Task1.V27
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #4 | Выполнил : Паутова М.О. | ИБКСб-25-1";
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #4                                                               *");
+            Console.WriteLine("* Тема:    одномерные массив                                              *");
+            Console.WriteLine("* Задание #2                                                              *");
+            Console.WriteLine("* Вариант #27                                                             *");
+            Console.WriteLine("* Выполнил: Паутова Мария Олеговна | ИБКСб-25-1                           *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("*  подсчитать произведение четных элементов массива.                      *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            int len;
+            Console.WriteLine(" Введите число элеметнов:  ");
+            len=Convert.ToInt32(Console.ReadLine());
+
+            int[] array = new int[len];
+            
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine("Введите "+i+" элемент");
+                array[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine(array[i] +"\t");
+            }
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            int res = ds.Calculate(array);
+            Console.WriteLine("Произведение четных элементов = " + res);
+
+
+            Console.ReadKey();
+        }
+    }
+}
