b6548b84ea8a7dd7628a6cc829c62d640d522299
PautovaMO <Pautova812007@mail.ru>
2025-11-09 11:36:31 +0500

    Task4

---
+++ Tyuiu.PautovaMO.Sprint4.Task4.V29.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using Tyuiu.PautovaMO.Sprint4.Task4.V29.Lib;
+
+namespace Tyuiu.PautovaMO.Sprint4.Task4.V29.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            int[,] matrix = { { 7, 5, 5, 6, 7 }, { 8, 7, 8, 4, 3 }, { 5, 6, 7, 8, 3 }, { 4, 2, 3, 6, 4 }, { 5, 2, 4, 2, 3 } };
+            int res = ds.Calculate(matrix);
+            int wait = 13;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.PautovaMO.Sprint4.Task4.V29/Program.cs
@@ -0,0 +1,68 @@
+﻿using System.Net;
+using Tyuiu.PautovaMO.Sprint4.Task4.V29.Lib;
+
+namespace Tyuiu.PautovaMO.Sprint4.Task4.V29
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #4 | Выполнил : Паутова М.О. | ИБКСб-25-1";
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #4                                                               *");
+            Console.WriteLine("* Тема:               массив                                              *");
+            Console.WriteLine("* Задание #4                                                              *");
+            Console.WriteLine("* Вариант #29                                                             *");
+            Console.WriteLine("* Выполнил: Паутова Мария Олеговна | ИБКСб-25-1                           *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("*  подсчитать произведение четных элементов массива.                      *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Введите количество строк: ");
+            int rows = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите количество столбцов: ");
+            int col = Convert.ToInt32(Console.ReadLine());
+            int[,] arr = new int[rows, col];
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < col; j++)
+                {
+                    Console.WriteLine($"Введите значение {i},{j} элемента: ");
+                    arr[i,j] = Convert.ToInt32(Console.ReadLine());
+
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("Массив\n");
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < col; j++)
+                {
+                    Console.WriteLine($"{arr[i, j]}\t");
+
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            int res = ds.Calculate(arr);
+            Console.WriteLine("Колличество = " + res);
+
+
+            Console.ReadKey();
+        }
+    }
+}
